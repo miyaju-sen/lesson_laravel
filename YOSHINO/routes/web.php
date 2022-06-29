90b6bd7594573092ab083ebf6c3b27f942602967
miyaju-sen <kawahara.hal@gmail.com>
2022-06-29 16:44:47 +0900

    list 3-1, 3-2

--- YOSHINO/routes/web.php
+++ YOSHINO/routes/web.php
@@ -18,5 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('hello', [HelloController::class, 'index']);
+Route::get('hello', function () {
+    return view('hello.index');
+});
