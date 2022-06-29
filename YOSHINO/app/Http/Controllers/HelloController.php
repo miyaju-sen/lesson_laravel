c47cbb9f8dea30e1de835ccb177a91cdc68a31ea
miyaju-sen <kawahara.hal@gmail.com>
2022-06-29 17:16:36 +0900

    list 3-10, 3-11

--- YOSHINO/app/Http/Controllers/HelloController.php
+++ YOSHINO/app/Http/Controllers/HelloController.php
@@ -9,11 +9,11 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
 
-    public function index($id='zero')
+    public function index(Request $request)
     {
         $data = [
             'msg' => 'これはコントローラから渡されたメッセージです。',
-            'id' => $id,
+            'id' => $request->id,   // ここの「id」は別に「id」である必要なし（クエリに合わせる。クエリがtestならここもtest）
         ];
         return view('hello.index', $data);  // 第二引数には連想配列を渡す
     }
