33c94370bb48d426dd1c9e9dd819f7d69ecf48c2
miyaju-sen <kawahara.hal@gmail.com>
2022-06-29 17:06:37 +0900

    list 3-7, 3-8, 3-9

--- YOSHINO/app/Http/Controllers/HelloController.php
+++ YOSHINO/app/Http/Controllers/HelloController.php
@@ -9,9 +9,12 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
 
-    public function index()
+    public function index($id='zero')
     {
-        $data = ['msg' => 'これはコントローラから渡されたメッセージです。'];
+        $data = [
+            'msg' => 'これはコントローラから渡されたメッセージです。',
+            'id' => $id,
+        ];
         return view('hello.index', $data);  // 第二引数には連想配列を渡す
     }
 }
