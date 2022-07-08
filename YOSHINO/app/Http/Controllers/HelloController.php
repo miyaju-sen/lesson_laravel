738e2da0613d54b2fad9d6ecb456cd9fd02ad2dc
miyaju-sen <kawahara.hal@gmail.com>
2022-07-08 14:27:28 +0900

    list 3-12, 3-13

--- YOSHINO/app/Http/Controllers/HelloController.php
+++ YOSHINO/app/Http/Controllers/HelloController.php
@@ -12,7 +12,7 @@ class HelloController extends Controller
     public function index(Request $request)
     {
         $data = [
-            'msg' => 'これはコントローラから渡されたメッセージです。',
+            'msg' => 'これはBladeを利用したサンプルです',
             'id' => $request->id,   // ここの「id」は別に「id」である必要なし（クエリに合わせる。クエリがtestならここもtest）
         ];
         return view('hello.index', $data);  // 第二引数には連想配列を渡す
