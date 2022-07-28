58ec73e0ec05d72378cfe54e7d3f38d27d4c0d6b
miyaju-sen <kawahara.hal@gmail.com>
2022-07-28 11:53:43 +0900

    list 4-9, 4-10

--- YOSHINO/routes/web.php
+++ YOSHINO/routes/web.php
@@ -20,5 +20,5 @@ Route::get('/', function () {
 });
 
 
-Route::get('hello', [HelloController::class, 'index'])->middleware(HelloMiddleware::class);
+Route::get('hello', [HelloController::class, 'index']);
 Route::post('hello', [HelloController::class, 'post']);
