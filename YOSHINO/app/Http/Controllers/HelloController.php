7288cda6d6b5329ea7d9819e8ebdd7ef3e01f760
miyaju-sen <kawahara.hal@gmail.com>
2023-01-30 15:07:21 +0900

    list 5-17

--- YOSHINO/app/Http/Controllers/HelloController.php
+++ YOSHINO/app/Http/Controllers/HelloController.php
@@ -14,7 +14,7 @@ class HelloController extends Controller
 
     public function index(Request $request)
     {
-        $items = DB::select('SELECT * FROM people');
+        $items = DB::table('people')->get();
         return view('hello.index', ['items' => $items]);
     }
 
