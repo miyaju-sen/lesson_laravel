5c2d97bbcf7b949d74dcb84eea2241c0c2b094d8
miyaju-sen <kawahara.hal@gmail.com>
2023-01-30 15:33:31 +0900

    list 5-23

--- YOSHINO/app/Http/Controllers/HelloController.php
+++ YOSHINO/app/Http/Controllers/HelloController.php
@@ -74,8 +74,13 @@ class HelloController extends Controller
     }
 
     public function show(Request $request) {
-        $id = $request->id;
-        $items = DB::table('people')->where('id', '<=', $id)->get();
+        $name = $request->name;
+        $name = '%'.$name.'%';
+
+        $items = DB::table('people')
+            ->where('name', 'like', $name)
+            ->orWhere('mail', 'like', $name)
+            ->get();
 
         return view('hello.show', ['items' => $items]);
     }
