b61cfe67eeb77f8164c3bffad5d0158d1d742e49
miyaju-sen <kawahara.hal@gmail.com>
2023-02-15 15:19:48 +0900

    list 6-13, 6-14

--- YOSHINO/app/Models/Person.php
+++ YOSHINO/app/Models/Person.php
@@ -14,4 +14,12 @@ class Person extends Model
     public function scopeNameEqual($query, $str) {
         return $query->where('name', $str);
     }
+
+    public function scopeAgeGreaterThan($query, $n) {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n) {
+        return $query->where('age', '<=', $n);
+    }
 }
