04682a32853a32ac1cc9cac08a2d212ee1b299d0
miyaju-sen <kawahara.hal@gmail.com>
2023-03-08 15:22:45 +0900

    list 6-18, 6-19, 6-20, 6-21

--- YOSHINO/app/Http/Controllers/PersonController.php
+++ YOSHINO/app/Http/Controllers/PersonController.php
@@ -26,4 +26,27 @@ class PersonController extends Controller
 
         return view('person.find', $param);
     }
+
+    public function add(Request $request) {
+        return view('person.add');
+    }
+
+    public function create(Request $request) {
+        // バリデーション実行
+        $this->validate($request, Person::$rules);
+
+        $person = new Person;
+
+        $form = $request->all();    // フォームの値を受け取る
+        unset($form['_token']);     // が、非表示フィールドは削除
+
+        // 個々のプロパティをまとめて設定
+        $person->fill($form)->save();
+
+        // 個別に設定する場合
+        // $person->name = $request->name;
+        // $person->save();
+
+        return redirect('/person');
+    }
 }
--- YOSHINO/app/Models/Person.php
+++ YOSHINO/app/Models/Person.php
@@ -10,13 +10,23 @@ use Illuminate\Database\Eloquent\Builder;
 class Person extends Model
 {
 
-    protected static function boot() {
-        // 初期化処理
-        parent::boot();
+    // protected static function boot() {
+    //     // 初期化処理
+    //     parent::boot();
 
-        // これがすべての検索処理に適用される（本テーブルから呼び出す際の前提となる）
-        static::addGlobalScope(new ScopePerson);
-    }
+    //     // これがすべての検索処理に適用される（本テーブルから呼び出す際の前提となる）
+    //     static::addGlobalScope(new ScopePerson);
+    // }
+
+    // 入力のガード（値を用意しておかない項目としてidを指定）
+    // idはDB側で自動的に番号を割り振るため、モデル作成時に値が必要ない
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150',
+    );
 
     public function getData() {
         return $this->id.': '.$this->name.'('.$this->age.')';
